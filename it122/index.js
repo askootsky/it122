65a6a0f5e7037407636e7790b8074db43f7c7f1f
Austin Skootsky <askootsky@yahoo.com>
2025-01-19 19:05:58 -0800

    Completed Assignment 2

--- it122/index.js
+++ it122/index.js
@@ -1,19 +1,46 @@
-import http from 'http';
+// Import from data.js
+import { getAllPokes, getOnePoke } from './data.js';
 
-http.createServer((req,res) => {
-    var path = req.url.toLowerCase();    
-    switch(path) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('This is my home page');
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('My name is Austin. I am in IT 122.');
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
-            break;
-    }    
-}).listen(process.env.PORT || 3000);
+//Import express
+'use strict'
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('./public'));
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+
+// Home Page route
+app.get('/', (req, res) => {
+    res.render('home', {pokes: getAllPokes()});
+});
+
+// Detail page
+app.get('/detail', (req, res) => {
+    console.log(req.query)
+    let result = getOnePoke(req.query.name);
+    res.render('detail', {
+        name: req.query.name,
+        result
+        }
+    );
+});
+
+// About Page route
+app.get('/about', (req, res) => {
+    res.status(200).send('My name is Austin. I am in IT 122.');
+});
+
+// 404 Error
+app.use((req, res) => {
+    res.status(404).send('Not found');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
